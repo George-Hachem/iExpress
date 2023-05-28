11516b286c11b50dd263611a59bbc27eca14bc7c
George-Hachem <78202573+George-H12@users.noreply.github.com>
2023-05-28 19:36:18 -0400

    Initial Commit

---
+++ iExpress/iExpressApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpressApp.swift
+//  iExpress
+//
+//  Created by George on 5/28/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
